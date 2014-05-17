246b1721817c6348d872fae1547827e2cce6579b
Ivan Fraixedes <ifcdev@gmail.com>
2014-05-17 20:19:39 +0100

    Removed blank spaces of blank line

--- src/constants.js
+++ src/constants.js
@@ -25,7 +25,7 @@ const anObj = new AConstructor('You should not change me');
 
 anObj.getName(); // returns 'You should not change me' 
 anObj.setName('I changed you'); // It compiles, because const relates to the created object's reference stored in it, no to the value
-    
+
 anObj = new AConstruactor('It is going to crash'); // It crashes at compilation time with Error: "SyntaxError: Assignment to constant variable."
 
 
