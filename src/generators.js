83479a4f414f87f252e1ed7d0a5d70db8ecb2bed
Ivan Fraixedes <ifcdev@gmail.com>
2014-05-17 20:19:48 +0100

    Added a generator that excutes a delated yield of an iterator object
    I added a generator that execute a delated yield (`yield *`) of an
    object which implements a interator interface; `next` method returning
    an object with `done` boolean property and `value`

--- src/generators.js
+++ src/generators.js
@@ -12,6 +12,19 @@ function *genSync2(start, end, step) {
   }
 }
 
+function *genSyncIterator(start, end) {
+  yield * {
+    counter: start,
+    next: function () {
+      if (this.counter >= end) {
+        return {done: true, value: undefined}; 
+      }
+
+      return {value: this.counter++, done: false };
+    }
+  };
+}
+
 function *genSyncComposer(start, end, step) {
   var gen1 = new genSync1(end);
 
@@ -20,6 +33,13 @@ function *genSyncComposer(start, end, step) {
   }
 }
 
+function *genSyncComposerIterator(start, end) {
+  var gen1 = new genSync1(end);
+
+  for (let value of gen1) {
+    yield * new genSyncIterator(value, end);
+  }
+}
 
 
 var results = [];
@@ -35,6 +55,15 @@ results = [];
 var gComposer = new genSyncComposer(0, 3, 1);
 
 for (let val of gComposer) {
+  results.push(val);
+}
+
+console.log(results); // [0, 1, 2, 1, 2, 2]
+
+results = [];
+var gComposerIterator = new genSyncComposerIterator(0, 3);
+
+for (let val of gComposerIterator) {
   results.push(val);
 }
 
